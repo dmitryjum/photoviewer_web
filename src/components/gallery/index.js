7c7d842b38082271fb8d3fbdc93be9b870af2343
Dmitry Jum <xfiler@ymail.com>
2020-03-17 19:06:48 -0400

    loads more images if the window is bigger than the list of images

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { Row, Col, Card, Spinner } from 'react-bootstrap'
 import { useSelector, useDispatch } from "react-redux";
 import { requestImages, addMoreImages } from "../../actions/gallery";
@@ -8,6 +8,7 @@ const Gallery = () => {
   const loading = useSelector(state => state.images.loading)
   const entriesCountInDB =  useSelector(state => state.images.entriesCount)
   const loadedImagesCount = images.length
+  const [paintedImagesCount, setPaintedImagesCount] = useState(0)
 
   const dispatch = useDispatch();
 
@@ -15,22 +16,27 @@ const Gallery = () => {
     e.stopPropagation()
     const currentCur = window.innerHeight + document.documentElement.scrollTop;
     const windowHeight = document.documentElement.offsetHeight;
-    console.log('loaded images count', loadedImagesCount);
-    console.log('entries count', entriesCountInDB);
-    console.log('loading', loading)
     if (loading === false && loadedImagesCount < entriesCountInDB && currentCur === windowHeight) {
       dispatch(addMoreImages());
     }
   }, [dispatch, loading, loadedImagesCount, entriesCountInDB])
   
   useEffect(() => {
-    if (entriesCountInDB === null) { dispatch(requestImages()) }
+    if (entriesCountInDB === null) {
+       dispatch(requestImages())
+    }
+    const windowHeight = window.innerHeight;
+    const galleryLoadedHeight = document.getElementById('gallery').offsetHeight;
+    if (loadedImagesCount > 0 && loadedImagesCount === paintedImagesCount &&
+      windowHeight > galleryLoadedHeight && loading === false) {
+      dispatch(addMoreImages())
+    }
     window.addEventListener("scroll", onScrollHandler);
     return () => {
       window.removeEventListener("scroll", onScrollHandler);
     }
     
-  }, [dispatch, onScrollHandler, entriesCountInDB])
+  }, [dispatch, onScrollHandler, entriesCountInDB, paintedImagesCount, loadedImagesCount, loading])
 
   function loadingSpinner() {
     if (loading) {
@@ -46,11 +52,11 @@ const Gallery = () => {
 
   return (
     <>
-      <Row>
+      <Row id="gallery">
         {images.map((image, id) => (
           <Col key={id}>
             <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={`${image.url}/200/200`} />
+              <Card.Img variant="top" src={`${image.url}/200/200`} onLoad={() => setPaintedImagesCount(paintedImagesCount + 1)} />
             </Card>
           </Col>
         ))}
